7f77842b27551766a0cadd1bc721e2ff1183220f
Nei tong <Hengneitong.keo@student.cadt.edu.kh>
2025-03-15 23:48:10 +0700

    Done Navbar

--- Real-Estate-app/src/App.jsx
+++ Real-Estate-app/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 function App() {
   
 
   return (
     <>
-      <div>
-
+      <div className='w-full overflow-hidden'>
+        <Header />
+        
       </div>
         
     </>
